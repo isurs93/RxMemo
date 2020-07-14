1943871070966728dd8e5dd42cc338d428245f18
isurs93 <loadofcastle@gmail.com>
2020-07-14 21:29:15 +0900

    Add ViewController and Controller Swift File

---
+++ RxMemo/ViewModel/MemoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoDetailViewViewController.swift
+//  RxMemo
+//
+//  Created by 박성주 on 2020/07/14.
+//  Copyright © 2020 박성주. All rights reserved.
+//
+
+import UIKit
+
+class MemoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
