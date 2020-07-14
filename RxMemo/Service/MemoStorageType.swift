027bb67915c136e7210c0b2e18465a067b26557c
isurs93 <loadofcastle@gmail.com>
2020-07-14 21:12:58 +0900

    cocoaPod Framework

---
+++ RxMemo/Service/MemoStorageType.swift
@@ -0,0 +1,25 @@
+//
+//  MemoStorageType.swift
+//  RxMemo
+//
+//  Created by 박성주 on 2020/07/14.
+//  Copyright © 2020 박성주. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol  MemoStorageType {
+    // 기본적인 CRUD
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
